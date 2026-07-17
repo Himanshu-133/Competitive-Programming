eeb69c2840653a87fb9713719db0158a30211521
Himanshu shekhar <147793215+Himanshu-133@users.noreply.github.com>
2026-07-17 23:52:26 +0530

    ✅ Defanging an IP Address | Easy | Java | Runtime: 1 ms (22.88%) | Memory: 43 MB | 2026-07-17

---
+++ leetcode/easy/1108-defanging-an-ip-address/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        Stack<Character> sb=new Stack<>();
+        for(char ch:address.toCharArray()){
+            if(ch!='.'){
+                sb.push(ch);
+            }else{
+                char a=ch;
+                sb.push('[');
+                sb.push(a);
+                sb.push(']');
+            }
+        }
+        Collections.reverse(sb);
+        StringBuilder ab=new StringBuilder();
+        while (!sb.isEmpty()) {
+    ab.append(sb.pop());
+}
+        return ab.toString();
+    }
+}
