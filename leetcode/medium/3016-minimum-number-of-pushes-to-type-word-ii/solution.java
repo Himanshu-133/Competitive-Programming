cd954ce8f677d3b9142ba802549fbdde5f354f75
Himanshu shekhar <147793215+Himanshu-133@users.noreply.github.com>
2026-07-31 23:36:56 +0530

    ✅ Minimum Number of Pushes to Type Word II | Medium | Java | Runtime: 10 ms (66.76%) | Memory: 47.9 MB | 2026-07-31

---
+++ leetcode/medium/3016-minimum-number-of-pushes-to-type-word-ii/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq=new int[26];
+        for(char ch:word.toCharArray()){
+            freq[ch-'a']++;
+        }
+        Arrays.sort(freq);
+        int ans=0,count=0,cost=1;
+        for(int i=25;i>=0;i--){
+            if(freq[i]==0)break;
+            ans+=freq[i]*cost;
+            count++;
+            if(count%8==0){
+                cost++;
+            }
+        }
+        return ans;
+    }
+}
