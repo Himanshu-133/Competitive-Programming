9e6f744bb6763a41c0978a5d58ca1449554e42bf
Himanshu shekhar <147793215+Himanshu-133@users.noreply.github.com>
2026-09-04 02:57:34 +0530

    ✅ Minimum Moves to Equal Array Elements II | Medium | Java | Runtime: 9 ms (97.70%) | Memory: 47.5 MB | 2026-09-03

---
+++ leetcode/medium/0462-minimum-moves-to-equal-array-elements-ii/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums); 
+        int median=nums[nums.length/2]; 
+        int moves=0; 
+        for(int x:nums){ 
+            moves+=Math.abs(x-median);
+            } 
+        return moves;
+    }
+}
