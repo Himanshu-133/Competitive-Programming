5b4847e1e1429ddcea46db5086e211f89e61f88b
Himanshu shekhar <147793215+Himanshu-133@users.noreply.github.com>
2026-09-06 01:13:23 +0530

    ✅ Smallest Stable Index II | Medium | Java | Runtime: 4 ms (81.27%) | Memory: 133.1 MB | 2026-09-05

---
+++ leetcode/medium/3904-smallest-stable-index-ii/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n=nums.length;
+        int[] res=new int[n];
+        res[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            res[i]=Math.min(nums[i],res[i+1]);
+        }
+        int resM=nums[0];
+        for(int i=0;i<n;i++){
+            resM=Math.max(resM,nums[i]);
+            if(resM-res[i]<=k){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
