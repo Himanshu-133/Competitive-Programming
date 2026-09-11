db413a4b14f542cb3fc2b9a4ca7486a535585afc
Himanshu shekhar <147793215+Himanshu-133@users.noreply.github.com>
2026-09-11 15:55:43 +0530

    ✅ Unique 3-Digit Even Numbers | Easy | Java | Runtime: 4 ms (86.93%) | Memory: 46.6 MB | 2026-09-11

--- leetcode/easy/3483-unique-3-digit-even-numbers/solution.java
+++ leetcode/easy/3483-unique-3-digit-even-numbers/solution.java
@@ -1,30 +1,24 @@
 class Solution {
-    public int totalNumbers(int[] d) {
-        int[] f = new int[10];
-        for (int x : d) f[x]++;
-
-        int ans = 0;
-
-        for (int last = 0; last <= 8; last += 2) {
-            if (f[last] == 0) continue;
-
-            f[last]--;
-
-            for (int first = 1; first <= 9; first++) {
-                if (f[first] == 0) continue;
-
-                f[first]--;
-
-                for (int mid = 0; mid <= 9; mid++)
-                    if (f[mid] > 0)
-                        ans += 1;
-
-                f[first]++;
+    public int totalNumbers(int[] digits) {
+        Set<Integer> sb=new HashSet<>();
+        int n=digits.length;
+        for(int i=0;i<n;i++){
+            if(digits[i]%2!=0){
+                continue;
             }
-
-            f[last]++;
+            for(int j=0;j<n;j++){
+                if(j==i){
+                    continue;
+                }
+                for(int k=0;k<n;k++){
+                    if(k==i || k==j || digits[k]==0){
+                        continue;
+                    }
+                    int num=digits[k]*100+digits[j]*10+digits[i];
+                    sb.add(num);
+                }
+            }
         }
-
-        return ans;
+        return sb.size();
     }
 }
